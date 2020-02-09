f0d60dce51dc28c7c3b58273cad491bc90fed611
Gabriella126 <60803372+Gabriella126@users.noreply.github.com>
2020-02-09 15:30:23 -0500

    Add files via upload

--- mailmechanics.js
+++ mailmechanics.js
@@ -6,7 +6,7 @@ Blueprint Mail App
 var index = 0;
 var nextSee = false; var deleteSee = false; var forwardSee = false; var replySee = false;
 
-document.addEventListener("keydown", event => {
+function choose(event){
     var key = event.which;
     switch (key) {
         case 37 : { deleteMail(); break; }
@@ -14,8 +14,9 @@ document.addEventListener("keydown", event => {
         case 39 : { nextMail(); break; }
         case 40 : { replyMail(); break; }
     }
-    document.getElementById("demo").innerHTML = "The Unicode KEY code is: " + key;
-});
+}
+
+window.document.addEventListener("keydown", choose); 
 
 function loadMSG(index) {
 var header;
@@ -25,23 +26,23 @@ var attachments;
 }
 
 function nextMail() {
-    if (!nextSee) { document.getElementById("delete").style.visibility = "visible"; nextSee = true; }
-    else { document.getElementById("delete").style.visibility = "hidden"; nextSee = false; }
-    var nextIndex = (index + 1) % (PUT HERE: num of emails);
+    if (!nextSee) { window.document.getElementById("next").style.visibility = "visible"; nextSee = true; }
+    else { window.document.getElementById("next").style.visibility = "hidden"; nextSee = false; }
+    var nextIndex = (index + 1) % (9);
     loadMSG(nextIndex);
 }
 
 function deleteMail() {
-    if (!deleteSee) { document.getElementById("delete").style.visibility = "visible"; deleteSee = true; }
-    else { document.getElementById("delete").style.visibility = "hidden"; deleteSee = false; }
+    if (!deleteSee) { window.document.getElementById("trash").style.visibility = "visible"; deleteSee = true; }
+    else { window.document.getElementById("trash").style.visibility = "hidden"; deleteSee = false; }
 }
 
 function replyMail() {
-    if (!replySee) { document.getElementById("delete").style.visibility = "visible"; replySee = true; }
-    else { document.getElementById("delete").style.visibility = "hidden"; replySee = false; }
+    if (!replySee) { window.document.getElementById("reply").style.visibility = "visible"; replySee = true; }
+    else { window.document.getElementById("reply").style.visibility = "hidden"; replySee = false; }
 }
 
 function forwardMail() {
-    if (!forwardSee) { document.getElementById("delete").style.visibility = "visible"; forwardSee = true; }
-    else { document.getElementById("delete").style.visibility = "hidden"; forwardSee = false; }
+    if (!forwardSee) { window.document.getElementById("forward").style.visibility = "visible"; forwardSee = true; }
+    else { window.document.getElementById("forward").style.visibility = "hidden"; forwardSee = false; }
 }
